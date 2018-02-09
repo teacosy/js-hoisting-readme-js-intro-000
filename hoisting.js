f356660df5095755111268e02299b202bfafb920
Lucy <marerescue@gmx.com>
2018-02-09 04:14:26 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,30 +1,28 @@
-function callMe() {
+var lyric = "maybe";
+function callMe()
+{
+console.log(lyric);
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
-  console.log("so call me");
+  console.log("so call me");}
   return lyric;
-  var lyric = "maybe";
-}
 
 
+crazy();
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+    console.log("hey!!!");
   }
+
 }
 
+var Name = "Cricky";
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
+  console.log(Name);
+  var sayMy = "Kristin";
+     console.log("Kristin");
 }
+  console.log(Name);
+
+
+// prints out undefined, returns undefined
